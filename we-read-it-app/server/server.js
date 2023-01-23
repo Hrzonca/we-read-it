d9ae6b54196637b43407213ee0e3d8218327a679
Haleigh Rzonca <hgielah@gmail.com>
2023-01-23 10:25:19 -0800

    trying to get the app runnning without changing anything yet

--- we-read-it-app/server/server.js
+++ we-read-it-app/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const { ApolloServer } = require('apollo-server-express');
+// const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '..client/public/index.html')));
 }
 
 app.use(routes);
