89fbaeff1c048e3624bddba23d23f80b0b9916ca
Haleigh Rzonca <hgielah@gmail.com>
2023-01-24 15:11:21 -0800

    completed searchbooks.js and working on server.js to get app running

--- my-app/client/src/pages/SearchBooks.js
+++ my-app/client/src/pages/SearchBooks.js
@@ -7,13 +7,15 @@ import { SAVE_BOOK } from "../utils/mutations";
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
-  // create state for holding returned google api data
+ 
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
+  
   const [searchInput, setSearchInput] = useState('');
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -30,7 +32,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(searchInput);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
