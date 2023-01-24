0d9885171000fe84e4eee8376f1faaec6288610b
Haleigh Rzonca <hgielah@gmail.com>
2023-01-23 17:09:35 -0800

    finished index.js in schemas and working on the resolvers.js middleware

---
+++ my-app/server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+const { Book, User } = require('../models');
+
+const resolvers = {
+    Query: {
+        user: async() => {
+            return User.find({})
+        },
+        book: async(parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return Book.find(params);
+        },
+    },
+    Mutation: {
+
+    }
+}
