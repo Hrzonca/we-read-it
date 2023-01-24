d516441badf5be6714716cb613af907672a96b25
Haleigh Rzonca <hgielah@gmail.com>
2023-01-24 11:42:03 -0800

    working on loginform. think it is finished but still have to test. also install apollo hooks

--- my-app/client/src/components/LoginForm.js
+++ my-app/client/src/components/LoginForm.js
@@ -1,14 +1,25 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from "@apollo/react-hooks";
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,26 +37,22 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        varible: { ...userFormData },
+      });
+      console.log(data);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      Auth.login(data.login.token);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  };
+  setUserFormData({
+    email: " ",
+    password: " ",
+  });
+
 
   return (
     <>
@@ -87,6 +94,6 @@ const LoginForm = () => {
       </Form>
     </>
   );
-};
+  };
 
 export default LoginForm;
