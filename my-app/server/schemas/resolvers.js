542d9084bad784bc278067f2232465e17095640e
Haleigh Rzonca <hgielah@gmail.com>
2023-01-25 18:39:25 -0800

    completed resolvers.js

--- my-app/server/schemas/resolvers.js
+++ my-app/server/schemas/resolvers.js
@@ -1,4 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -13,5 +15,54 @@ const resolvers = {
     },
     Mutation: {
         //todo login, addUser, saveBook
-    }
-}
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            
+            if (!user) {
+                throw new AuthenticationError("wrong email")
+            }
+
+            const correctPass = await user.isCorrectPass(password);
+
+            if (!correctPass) {
+                throw new AuthenticationError("Wrong password.")
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async(parent, { bookData }, context) => {
+            if (context.user) {
+                const updateUserBooks = await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$push: {savedBooks: bookData}},
+                    {new: true}
+                );
+
+                return updateUserBooks;
+            }
+            throw new AuthenticationError("Please login to save this book.")
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+              );
+      
+              return updatedUser;
+            }
+      
+            throw new AuthenticationError("You need to be logged in!");
+          },
+        },
+    };
+
+    module.exports = resolvers;
